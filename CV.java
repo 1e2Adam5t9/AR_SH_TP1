5b7b1afbd8e7628478beb2d71097ab98794dfb80
Adam Roy <adam_the_dud@hotmail.com>
2018-02-02 11:58:28 -0500

    Ajout du constrcteur par défaut

--- CV.java
+++ CV.java
@@ -4,5 +4,15 @@ public class CV {
 
 	public static void main( String[] args ) {
 		System.out.println( "Bienvenue chez Barette!" );
+
+	}
+
+	public CV() {
+		this.nom = "";
+		this.prenom = "";
+		this.formation = "";
+		this.attentes = "";
+		this.anneesExperience = 0;
 	}
 }
+
