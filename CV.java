a6fe6dede4d6222ae95626fd333526ac8a0e50c2
Adam Roy <adam_the_dud@hotmail.com>
2018-02-02 12:12:29 -0500

    Ajout du constructeur avec paramètres, les variables et la méthode affiche()

--- CV.java
+++ CV.java
@@ -14,5 +14,26 @@ public class CV {
 		this.attentes = "";
 		this.anneesExperience = 0;
 	}
+
+	//constructeur avec paramètres
+	public CV( String nom, String prenom, String formation, String attentes, String[] tabCompetences, int anneesExperience ) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+		this.formation = formation;
+		this.attentes = attentes;
+		this.tabCompetences = tabCompetences;
+		this.anneesExperience = anneesExperience;
+	}
+
+	public String nom, prenom, formation, attentes;
+	public String[] tabCompetences;
+	public int anneesExperience;
+
+	public void affiche() {
+		System.out.println( this.nom + "\n" + this.prenom + "\n" + this.formation + "\n" + this.attentes + "\n" + this.anneesExperience );
+		for ( int i = 0; i < this.tabCompetences.length; i++ ) {
+			System.out.println( "\n" + this.tabCompetences[i] );
+		}
 }
 
